8beb3f8e757335d51d9e4a50367bb7dd4ccd9c30
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:20:20 +0530

    Create  DoWhileLoop.java

---
+++ Loops/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("Numbers from 1 to 5:");
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+}
