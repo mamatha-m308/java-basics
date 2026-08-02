49e695359a3e48aca4731183451555a191fbce58
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:26:52 +0530

    Create ReturnValues.java

---
+++ Methods/ReturnValues.java
@@ -0,0 +1,13 @@
+public class ReturnValues {
+     public static void main(String[] args) {
+
+        int sum = add(10, 20);
+
+        System.out.println("Sum = " + sum);
+    }
+
+    // Method with return value
+    public static int add(int a, int b) {
+        return a + b;
+    } 
+}
