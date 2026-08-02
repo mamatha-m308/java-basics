82812d6d66a574f4beac4c4a32b342f686f58b10
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:25:43 +0530

    Create MethodParameters.java

---
+++ Methods/MethodParameters.java
@@ -0,0 +1,10 @@
+public class MethodParameters {
+       public static void main(String[] args) {
+        displayDetails("Mamatha", 20);
+    }
+    // Method with parameters
+    public static void displayDetails(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
