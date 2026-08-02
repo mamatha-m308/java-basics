075191f45c683e4469b1a331907fd505818b6d7f
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:19:29 +0530

    Add WhileLoop class to print numbers from 1 to 5

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("Numbers from 1 to 5:");
+
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
