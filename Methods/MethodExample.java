dee7ac3302f8217754ba18133136eb3d4f6e03cb
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:24:34 +0530

    Remove duplicate main method in MethodExample

---
+++ Methods/MethodExample.java
@@ -0,0 +1,9 @@
+public class MethodExample {
+     public static void main(String[] args) {
+        greet();
+    }
+    // Method
+    public static void greet() {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
