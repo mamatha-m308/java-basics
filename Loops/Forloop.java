4193ac413e91591ec15ef2ebc3c059be87ba7883
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:18:45 +0530

    Update Forloop.java

--- Loops/Forloop.java
+++ Loops/Forloop.java
@@ -1,1 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
 
+        System.out.println("Numbers from 1 to 5:");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
