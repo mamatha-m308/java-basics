a7e7a4c4a0f0f3628f08b8b256714324b1373db6
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-02 14:30:57 +0530

    Create VoidMethod.java

---
+++ Methods/VoidMethod.java
@@ -0,0 +1,11 @@
+public class VoidMethod {
+         public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("This method does not return any value.");
+    }
+
+
+}
