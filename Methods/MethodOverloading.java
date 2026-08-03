5768acc07a8debb4c46843591880f01f6e3df5b1
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-03 11:56:17 +0530

    Add method overloading examples in funoverloading class

--- Methods/MethodOverloading.java
+++ Methods/MethodOverloading.java
@@ -1,17 +1,27 @@
-public class MethodOverloading {
-        public static void main(String[] args) {
-
-        System.out.println(add(10, 20));
-        System.out.println(add(10.5, 20.5));
-    }
-
-    // Method 1
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    // Method 2
-    public static double add(double a, double b) {
-        return a + b;
-    }
+public class funoverloading {
+ 
+	public static void main(String[] args) {
+		fun(3.4,3.5);
+		fun(1,2);
+		fun("mamatha");
+		fun("mamathabm",10);
+	}
+	
+	static void fun(int a,int b) {
+		int sum=a+b;
+		System.out.println("sum is:"+sum);
+	}
+	
+	static void fun(double a,double b) {
+		double sum=a+b;
+		System.out.println("sum of this:"+sum);
+	}
+	
+	static void fun(String name) {
+		System.out.println(name);
+	}
+	
+	static void fun(String name,int a) {
+		System.out.println(name+" "+a);
+	}
 }
