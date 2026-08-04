7262e3ef8897dd1219d83eefcf02d42df945f878
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-04 11:18:19 +0530

    Add ArrayList operations in Arraylist.java
    Implement basic operations on an ArrayList including addition, update, and removal of elements.

---
+++ Arraylist/Arraylist.java
@@ -0,0 +1,41 @@
+
+//we use array list when we don't know the size of an array
+public class Arraylist {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	//Syntax 
+	ArrayList<Integer> list=new ArrayList<>();
+	
+	list.add(27);
+	list.add(32);
+	list.add(54);
+	list.add(56);
+	list.add(87);
+	list.add(98);
+	
+	System.out.println(list.contains(54));
+	System.out.println(list.contains(433));
+	System.out.println(list);
+	
+	//update array list
+	list.set(0, 99);
+	System.out.println(list);
+	{
+	//remove from array list
+	list.remove(2);
+	System.out.println(list);
+	
+	
+	//input for array list
+//	for(int i=0; i<5;i++) {
+//   list.add(sc.nextInt());
+//	      }
+	
+//	for(int i=0; i<5; i++) {
+//		System.out.println(list.get(i));      pass index here
+//	}
+	
+}
+}
+}
