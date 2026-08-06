95d14e9904c69800eddc81ade224b58436e6b69e
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-06 22:14:41 +0530

    Add min function to find minimum in an array
    Implement a method to find the minimum value in an array.

---
+++ Arrays/mineleinarray.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+public class mineleinarray {
+public static void main(String[] args) {
+	int[] nums={43,32,54,75,12,-45,11};
+	
+	int result=min(nums);
+	System.out.println(result);
+}
+static int min(int[] nums) {
+	if(nums.length==0) {
+		return -1;
+	}
+	
+	int min=nums[0];
+	for(int i=0; i<nums.length; i++) {
+		if(nums[i]<min) {
+			min=i;
+		}
+	}
+return min;
+}
+}
