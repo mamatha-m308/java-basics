be3427cfcbecdef2fcc12ca1766ddcef7b6a1801
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-17 22:02:02 +0530

    Update multidimentionarrlis.java

--- Arraylist/multidimentionarrlis.java
+++ Arraylist/multidimentionarrlis.java
@@ -1,4 +1,4 @@
-public class multidimentionarrlist {
+ public class multidimentionarrlist {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	ArrayList<ArrayList<Integer>> list=new ArrayList<>();	
