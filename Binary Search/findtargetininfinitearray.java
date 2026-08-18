17f6fee5604087699bcf9d19e5a1100dcac6c113
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-18 13:22:52 +0530

    Create findtargetininfinitearray.java

---
+++ Binary Search/findtargetininfinitearray.java
@@ -0,0 +1,53 @@
+package Binarysearch;
+
+public class findtargetininfinitearray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 5, 7, 8, 12, 23, 26, 29, 30, 38, 49};
+
+        int target = 7;
+
+        System.out.println(ans(arr, target));
+    }
+
+    static int ans(int[] arr, int target) {
+
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+
+            int temp = end + 1;
+
+            end = end + (end - start) * 2;
+
+            start = temp;
+        }
+
+        return binarysearch(arr, target, start, end);
+    }
+
+    static int binarysearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (target < arr[mid]) {
+
+                end = mid - 1;
+
+            } else if (target > arr[mid]) {
+
+                start = mid + 1;
+
+            } else {
+
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}v
