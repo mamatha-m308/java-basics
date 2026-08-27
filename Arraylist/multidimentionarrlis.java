602604429403587c44011b8a78a9a437e67b1582
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-08-27 22:07:15 +0530

    Update multidimentionarrlis.java

--- Arraylist/multidimentionarrlis.java
+++ Arraylist/multidimentionarrlis.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 		list.add(new ArrayList<>());
 	}
 	
-	//add elements
+	//add element s
 	for(int i=0; i<3; i++) {
 		for(int j=0; j<3; j++) {
 			list.get(i).add(sc.nextInt());
